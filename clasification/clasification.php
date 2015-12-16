33316ff989b9e4fdf3d3e7a953865922cb8f10ed
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2015-12-16 19:03:43 +0100

    Create clasification PHP Web page

---
+++ clasification/clasification.php
@@ -0,0 +1,29 @@
+<?php
+    // SESION GOES HERE
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Clasificación</title>
+        <!-- CSS GOES HERE -->
+    </head>
+    <body>
+        <?php
+            include 'credentials.php';
+                    
+            $conexion = new mysqli('$HOST', '$USER', '$PASS', '$DB');
+
+            if ($conexion->connect_error) {
+                die("Error de conexión: " . $conexion->connect_error);
+            }
+        ?>
+        <!-- HEADER START -->
+        <!-- HEADER END -->
+        <?php
+            
+        ?>
+        <!-- FOOT START -->
+        <!-- FOOT END -->
+    </body>
+</html>
