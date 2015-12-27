402679f29f01b9bae802222675f9ccf8e295562d
Juan Miguel Navarro Martínez <juanmi.3000@gmail.com>
2015-12-27 17:53:45 +0100

    Use bootstrap locally for HTTPS

--- clasification/clasification.php
+++ clasification/clasification.php
@@ -6,9 +6,9 @@ require '../config.php';
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="../css/bootstrap.min.css">
+		<script src="../js/jquery-2.1.4.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
 		<title>Clasificación</title>
 		<!-- CSS GOES HERE -->
 		<style>
