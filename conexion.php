db9b9482056d90b32c9c541af2bfe6213fa5d718
Andoni Alonso Fernandez <andonialonsof@gmail.com>
2016-01-13 17:58:30 +0100

    Fixed connexion BD

--- conexion.php
+++ conexion.php
@@ -5,10 +5,10 @@
 * Toma los valores del fichero config.php 
 */
 
-include dirname(__FILE__).'/../config.local.php';
-$conexion = new mysqli($host, $username, $password, $db_name);
+require './config.local.php';
+$conexion = new mysqli($config['dbHost'], $config['dbUser'], $config['dbPass'], $config['dbName']);
 if ($conexion->connect_errno) { // Si se produce algún error finaliza con mensaje de error
-die("Error de Conexión: " . $conexion->connect_error);
+die("Error de Conexión: ". $conexion->connect_error);
 }
 $conexion->set_charset("utf8");
 
