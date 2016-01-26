e8825cd6ddef35004b04741f03033cd6f24b77e4
Andoni Alonso Fernandez <andonialonsof@gmail.com>
2016-01-26 18:04:30 +0100

    Add hasher.php

---
+++ hasher.php
@@ -0,0 +1,21 @@
+<?php
+
+$pass1 = password_hash("ausias", PASSWORD_DEFAULT);
+$pass2 = password_hash("ausias", PASSWORD_DEFAULT);
+
+$query = "UPDATE `users` "
+		. "SET `userPass`=? "
+		. "WHERE `userNick='ausias'";
+
+$stmt=$conexion->prepare($query);
+$stmt->bind_param('s',$pass1);
+$stmt->execute();
+$stmt->close();
+
+$query2 = "UPDATE `users` "
+		. "SET `userPass`=? "
+		. "WHERE `userNick='SiriusBnW'";
+$stmt=$conexion->prepare($query2);
+$stmt->bind_param('s',$pass2);
+$stmt->execute();
+$stmt->close();
